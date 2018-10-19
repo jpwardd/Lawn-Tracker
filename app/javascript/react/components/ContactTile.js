2bcaf790cd02d1b0b1053b5fd048dfd3ff254a54
jpwardd <jward9421@gmail.com>
2018-10-19 07:57:11 -0400

    Added material-ui and react 16

--- app/javascript/react/components/ContactTile.js
+++ app/javascript/react/components/ContactTile.js
@@ -3,8 +3,9 @@ import React from 'react'
 
 const ContactTile = (props) => {
   return(
-    <div className="mb-5 card mx-auto container bg-dark">
-      <div className="card-body">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
         <ul className="list-group list-group-flush">
           
           <h6 className="text-white">first name:</h6>
@@ -13,24 +14,25 @@ const ContactTile = (props) => {
           <h6 className="text-white">last name:</h6>
             <li className="list-group-item">{props.lastName}</li>
           
-          <h6 className="text-white">Phone:</h6>
+          <h6 className="text-dark">Phone:</h6>
             <li className="list-group-item">{props.phoneNumber}</li>
           
-          <h6 className="text-white">Email:</h6>
+          <h6 className="text-dark">Email:</h6>
             <li className="list-group-item">{props.email}</li>
           
-          <h6 className="text-white">Address:</h6>
+          <h6 className="text-dark">Address:</h6>
             <li className="list-group-item">{props.address}</li>
           
-          <h6 className="text-white">City:</h6>
+          <h6 className="text-dark">City:</h6>
             <li className="list-group-item">{props.city}</li>
           
-          <h6 className="text-white">state</h6>
+          <h6 className="text-dark">state</h6>
             <li className="list-group-item">{props.state}</li>
           
-          <h6 className="text-white">Zip Code:</h6>
+          <h6 className="text-dark">Zip Code:</h6>
             <li className="list-group-item">{props.zipCode}</li>
         </ul>
+        </div>
       </div>
     </div>
   )
