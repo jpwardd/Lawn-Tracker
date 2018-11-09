a9412f646a2abef5bae50808a1c285487f399775
jpwardd <jward9421@gmail.com>
2018-11-09 16:38:56 -0500

    Weather parser poro  update

--- app/models/weather_parser.rb
+++ app/models/weather_parser.rb
@@ -2,4 +2,9 @@ require "httparty"
 
 class WeatherParser
 
+  def search(query)
+    response = HTTParty.get("https://api.darksky.net/forecast/#{ENV["WEATHER_KEY"]}/37.8267,-122.4233")
+  end
+
+
 end
